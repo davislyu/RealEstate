7117d6b250f731d63b601168bdd285d1312cc1f5
davislyu <davislyu@gmail.com>
2023-11-05 08:59:43 +0200

    Connection to Mongo

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from "express";
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config();
 
+mongoose.connect(process.env.MONGO_CS).then(() => {
+  console.log("Connected to Mongo")
+}).catch((err) => {
+  console.log(`error : ${err}`)
+})
 const app = express();
 
 app.listen(3000, () => {
