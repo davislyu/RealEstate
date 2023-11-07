315cca55d3c02088e97532b0eafd63f6492db46a
davislyu <davislyu@gmail.com>
2023-11-07 20:35:29 +0200

    Added redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,75 +1,55 @@
-import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  signInStart,
-  signInSuccess,
-  signInFailure,
-} from "../redux/user/userSlice";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       dispatch(signInStart());
-
       const res = await fetch("/api/auth/signin", {
-        // The fetch call triggers the app.use("/api/auth",authRouter) from the index.js in the api side.
-        //It send a POST request with a content type header and a body. The body is a strigified json data which in our case is formData that we took as a state.
-
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
-
-      const data = await res.json(); //data is used to extract an error message if the request was not successful (i.e., res.ok is false).
-      // If there is an error, it throws an error with the message from data.
-      // This error is then caught in the catch block, where SetError is called with the message from the error,
-      //allowing it to be displayed to the user. If there is no error, it implies the operation was successful,
-      // and the navigation to the sign-in route is triggered.
+      const data = await res.json();
       console.log(data);
       if (data.success === false) {
         dispatch(signInFailure(data.message));
         return;
       }
-      dispatch(signInSuccess(data.message));
-
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      // Set the error message from the catch block
       dispatch(signInFailure(error.message));
     }
   };
-
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input
-          type="text"
-          placeholder="Email"
-          className="border p-3 rounded-lg"
-          id="email"
-          onChange={handleChange}
-        />
+        <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" onChange={handleChange} />
         <input
           type="password"
-          placeholder="Password"
+          placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
           onChange={handleChange}
         />
+
         <button
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
@@ -77,10 +57,10 @@ export default function SignIn() {
           {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
-      <div className=" flex gap-2 mt-5">
+      <div className="flex gap-2 mt-5">
         <p>Dont have an account?</p>
-        <Link to="/sign-up">
-          <span className="text-blue-700">Sign Up </span>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign up</span>
         </Link>
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
